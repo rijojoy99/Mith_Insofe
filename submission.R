6efabf7816b4b2dda34da73e3fc8a36febfa8670
rijojoycgi <t_rijo_joy@outlook.com>
2017-12-16 10:53:38 +0530

    Update submission.R

--- submission.R
+++ submission.R
@@ -2,9 +2,23 @@
 rm(list =ls(all= T))
 setwd("E:\\Myth_Insofe")
 
+library(lubridate)
 student_data <- read.csv("train.csv",header = T , sep = ",")
 
 str(student_data)
 summary(student_data)
 
 View(student_data)
+
+pMiss <- function(x){sum(is.na(x))/length(x)*100}
+apply(student_data,2,pMiss)
+
+
+# Dropping off ID columns as they do not impact the Model development
+student_data$Candidate.ID<- NULL
+str(student_data)
+# student_data$age <- 
+student_data$dob <- as.Date(substr(as.character(student_data$Date.Of.Birth), 1, 10),format = "%m/%d/%Y")
+student_data$age <-  (as.numeric(student_data$Year.of.Graduation.Completion) -  as.numeric(lubridate::year(student_data$dob)))
+
+str(student_data)
