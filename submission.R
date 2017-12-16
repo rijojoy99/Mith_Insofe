2ca0a0967204cdbb7194742d0975781c67387f23
rijojoycgi <t_rijo_joy@outlook.com>
2017-12-16 12:46:50 +0530

    Update submission.R

--- submission.R
+++ submission.R
@@ -1,8 +1,12 @@
 
 rm(list =ls(all= T))
 # setwd("E:\\Myth_Insofe")
+source("output_mith.log", echo=T)
 
 library(lubridate)
+library(Smisc)
+library(laeken)
+library(DMwR)
 student_data <- read.csv("train.csv",header = T , sep = ",")
 
 str(student_data)
@@ -23,31 +27,37 @@ student_data[,var] <- sapply(student_data[,var],function(x) ifelse(x==-1,NA,x))
 apply(student_data,2,pMiss_na)
 apply(student_data,2,pMiss_null)
 
-
-# Dropping off ID columns as they do not impact the Model development
-student_data$Candidate.ID<- NULL
-str(student_data)
-summary(student_data)
+# Found its more meaningfull to add this column into the dataset
 # student_data$age <- 
-student_data$dob <- as.Date(substr(as.character(student_data$Date.Of.Birth), 1, 10),format = "%m/%d/%Y")
-
-student_data$age <-  (as.numeric(student_data$Year.of.Graduation.Completion) -  as.numeric(lubridate::year(student_data$dob)))
-
-student_data$Date.Of.Birth <- NULL
-str(student_data)
-
+student_data$age <-  (as.numeric(student_data$Year.of.Graduation.Completion) -  as.numeric(year(lubridate::mdy(formatDT(c(student_data$Date.Of.Birth), posix = FALSE)$date))))
 
 # Convert to factors 
 # ->CollegeCode,CollegeTier,Year.of.Graduation.Completion,CityCode,CityTier,Year.of.Graduation.Completion
 
 student_data$CollegeCode <- as.character(student_data$CollegeCode)
 student_data$CollegeTier <- as.character(student_data$CollegeTier)
-student_data$Year.of.Graduation.Completion <- as.character(student_data$Year.of.Graduation.Completion)
+student_data$Year.Of.Twelth.Completion <- as.character(student_data$Year.Of.Twelth.Completion)
 student_data$CityCode <- as.character(student_data$CityCode)
 student_data$CityTier <- as.character(student_data$CityTier)
 student_data$Year.of.Graduation.Completion <- as.character(student_data$Year.of.Graduation.Completion)
+student_data$Date.Of.Birth <- as.character(student_data$Date.Of.Birth)
+summary(student_data) 
+
+
+# Dropping off ID columns as they do not impact the Model development
+#Age is calculated- removing DOB
+student_data$Date.Of.Birth <- NULL
+student_data$Candidate.ID<- NULL
+str(student_data)
 summary(student_data)
-student_data$dob<- NULL
+
 write.csv(x = student_data,file = "After_NA.csv")
+
+#Impute NA's in the dataset
+
+student_data<-centralImputation(student_data)
+apply(student_data,2,pMiss_na)
+
+summary(student_data)
 
 
